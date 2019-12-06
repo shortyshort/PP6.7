2eba300cb4290c88b93d198088ead230a22e562a
91068412 <isafra3.02@gmail.com>
2019-12-06 13:17:48 -0800

    Initial commit

---
+++ src/myGroceries.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.ListIterator;
+
+public class myGroceries
+{
+    public static void main (String[] args)
+    {
+        ArrayList<String> items = new ArrayList<String>();
+        items.add("Carrots");
+        items.add("meat");
+        items.add("drink");
+        System.out.println ("There are ");
+
+    }//end main method
+}//end class
+//Isabela Francisco
